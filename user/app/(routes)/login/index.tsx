5fbdc2c06af0c45c4cc379c8f7382ef555965adc
ak220193 <mail2meak22frcrio@gmail.com>
2025-04-25 09:18:25 +0530

    Auth Screen Pending

--- user/app/(routes)/login/index.tsx
+++ user/app/(routes)/login/index.tsx
@@ -1,10 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import LoginScreen from "@/screens/login/login.screen"
 
 export default function index() {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+   <LoginScreen />
   )
 }

3ec3b2192c28ebffeab1dbefb94c45485808f68d
ak220193 <mail2meak22frcrio@gmail.com>
2025-04-27 07:40:44 +0530

    Register user api created

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route";
 
 
 export const app = express();
@@ -10,6 +11,9 @@ app.use(express.json({ limit: "50mb" }));
 
 // cookie parserv
 app.use(cookieParser());
+
+// routes
+app.use("/api/v1", userRouter);
 
 app.get("/test", (req: Request,res:Response,next:NextFunction) => {
     res.status(200).json({

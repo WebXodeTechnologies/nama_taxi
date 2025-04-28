1da2e9ff38cf6338d7fb671f0ef1821e40f00d5a
ak220193 <mail2meak22frcrio@gmail.com>
2025-04-28 18:28:58 +0530

    changes for Customer Registration and controller Logic

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import { NextFunction, Request, Response } from "express";
 import twilio from "twilio";
 
-
 dotenv.config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -27,7 +26,7 @@ export const registerUser = async (
           channel: "sms",
           to: phone_number,
         });
-        
+
       return res.status(201).json({ success: true });
     } catch (error) {
       console.log(error);
@@ -35,28 +34,38 @@ export const registerUser = async (
     }
   } catch (error) {
     console.log(error);
-    return res.status(400).json({ success: false, message:"something went wrong"});
+    return res
+      .status(400)
+      .json({ success: false, message: "something went wrong" });
   }
 };
 
+// verify OTP Function
 
 export const verifyOtp = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  
- try {
-  const {phone_number,otp} = req.body;
   try {
-    await client.verify.v2?.services(process.env.SERVICE_SID!).verificationChecks.create({ to: phone_number, code: otp })
-    return res.status(200).json({success:true, message:"otp verified Successfully"})
+    const { phone_number, otp } = req.body;
+    try {
+      await client.verify.v2
+        ?.services(process.env.SERVICE_SID!)
+        .verificationChecks.create({ to: phone_number, code: otp });
+      return res
+        .status(200)
+        .json({ success: true, message: "otp verified Successfully" });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(400)
+        .json({ success: false, message: "something went wrong" });
+    }
   } catch (error) {
     console.log(error);
-    return res.status(400).json({success:false,message:"something went wrong"});
+    return res
+      .status(400)
+      .json({ success: false, message: "something went wrong" });
   }
- } catch (error) {
-  console.log(error);
-  return res.status(400).json({success:false, message:"something went wrong"})
- }
 };

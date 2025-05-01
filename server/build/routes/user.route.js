ef3dec7e2aab0afb4f3ccfc45912deb53872d3b3
ak220193 <mail2meak22frcrio@gmail.com>
2025-05-01 09:57:22 +0530

    Bug Fix in Controller and prisma Logic - all bug issue rectified for verify otp Controller

---
+++ server/build/routes/user.route.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const user_controller_1 = require("../controllers/user.controller");
+const userRouter = express_1.default.Router();
+userRouter.post("/registration", user_controller_1.registerUser);
+userRouter.post("/verify-otp", user_controller_1.verifyOtp);
+exports.default = userRouter;

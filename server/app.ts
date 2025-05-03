3165bb29fc4c487e7e4806661e6c64b186fcafdc
ak220193 <mail2meak22frcrio@gmail.com>
2025-05-03 09:54:20 +0530

    verification Complete

--- server/app.ts
+++ server/app.ts
@@ -2,9 +2,13 @@ require("dotenv").config();
 import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route";
-
+import Nylas from "nylas";
 
 export const app = express();
+
+const nylas = new Nylas({
+    apiKey: process.env.NYLAS_API_KEY!,
+  });
 
 // body parser
 app.use(express.json({ limit: "50mb" }));

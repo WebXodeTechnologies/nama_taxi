3caf6fc7c629895a44f8de4dc807aed5854cf72a
ak220193 <mail2meak22frcrio@gmail.com>
2025-05-05 11:42:21 +0530

    Verify Email Controller Logic Complete

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,12 +1,13 @@
 import express from "express"
-import {registerUser, sendingOtpToEmail, signupNewUser, verifyOtp} from "../controllers/user.controller"
+import {registerUser, sendingOtpToEmail, verifyingEmail, verifyOtp} from "../controllers/user.controller"
 
 
 const userRouter = express.Router();
 
 userRouter.post("/registration", registerUser);
 userRouter.post("/verify-otp", verifyOtp);
-userRouter.put("/sign-up-user", signupNewUser);
 userRouter.post("/email-otp-request", sendingOtpToEmail);
+userRouter.put("/email-otp-verify",verifyingEmail);
+
 
 export default userRouter;

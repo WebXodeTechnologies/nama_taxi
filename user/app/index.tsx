206e668737c09f6403573075f2467c3487bc3b6d
ak220193 <mail2meak22frcrio@gmail.com>
2025-05-22 10:42:03 +0530

    Ride Plan Screen is pending

--- user/app/index.tsx
+++ user/app/index.tsx
@@ -2,30 +2,36 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Redirect } from "expo-router";
 import { useEffect, useState } from "react";
 
-
 export default function Index() {
   const [isLoggedIn, setisLoggedIn] = useState(false);
-  const [isLoading,setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(()=> {
+  useEffect(() => {
+    let isMounted = true;
     const getData = async () => {
-       try {
-      const accessToken = await AsyncStorage.getItem("accessToken");
-      if(accessToken) {
-        setisLoggedIn(true)
-      } else {
-        setisLoggedIn(false);
+      try {
+        const accessToken = await AsyncStorage.getItem("accessToken");
+        if (accessToken) {
+          setisLoggedIn(true);
+        } else {
+          setisLoggedIn(false);
+        }
+      } catch (error) {
+        console.log("Failed to retrieve accessToken", error);
+      } finally {
+        if (isMounted) {
+          setIsLoading(false);
+        }
       }
-    } catch (error) {
-      console.log("Failed to retrieve accessToken", error)
-    } finally {
-      setIsLoading(false);
-    }}
+    };
     getData();
-  },[])
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-    if (isLoading) {
-    return null; 
+  if (isLoading) {
+    return null;
   }
   return (
     <Redirect href={!isLoggedIn ? "/(routes)/onboarding" : "/(tabs)/home"} />
